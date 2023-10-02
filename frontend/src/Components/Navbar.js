6972dd666a628af2c4b758be88ab26dca0122775
markoka101 <markoka101@gmail.com>
2023-10-02 16:11:44 -0400

    added files for home page and navbar for frontend

---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,7 @@
+import { ArrowRightIcon } from "@heroicons/react/solid";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Navbar() {
+    return null;
+}
