e9e8df9997ec59df46f9b32100aa327dfed5fa64
markoka101 <markoka101@gmail.com>
2023-10-05 19:14:38 -0400

    added cookie functionality to frontend and crossorigin  to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,32 @@ import {
     BrowserRouter as Router,
     Routes,
     Route
-   } from "react-router-dom";
+} from "react-router-dom";
+import { CookiesProvide, CookiesProvider, useCookies } from "react-cookie";
+
+//import pages
 import LoginPage from "./Components/LoginPage";
 import RegisterPage from "./Components/RegisterPage";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 
 export default function App() {
-    return null;
+
+    const [cookies, setCookie] = useCookies(["user"]);
+
+    function handleLogin(user) {
+        setCookie('user', user, { path: '/'});
+    }
+
+    return (
+        <CookiesProvider>
+            <div>
+                {cookies.user ? (
+                    <Home user = {cookies.user} />
+                ) : (
+                    <LoginPage onLogin={handleLogin} />
+                )}
+            </div>
+        </CookiesProvider>
+    )
 }
--- frontend/src/Components/LoginPage.js
+++ frontend/src/Components/LoginPage.js
@@ -1,2 +1,57 @@
 import React from "react";
+import { useState } from "react";
 import {Link} from "react-router-dom";
+
+export default function LoginPage({ onLogin }) {
+
+    //setting states for form
+    const [username,setUsername] = React.useState('');
+    const [password,setPassword] = React.useState('');
+
+    //user object
+    const userObj = {
+        username: username,
+        password: password
+    };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch('http://localhost:8080/user/login', ({
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userObj)
+        }))
+        .then(res => {
+            if (res.status === 202) {
+                res.json()
+                .then(data => {
+                    const id = data.id;
+                    const token = data.accessToken; 
+                    onLogin({id, token});
+                })
+            } else {
+                alert('username or password is incorrect');
+            }
+        })
+        .catch(err => console.log(err));
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type="text" placeholder="username" value={username}
+                onChange={(e) => setUsername(e.target.value)}/>
+            </label>
+            <label>
+                Password:
+                <input type="password" placeholder="password" value={password}
+                onChange={(e) => setPassword(e.target.value)}/>
+            </label>
+            <input type="submit" value="Submit"/>
+        </form>
+    );
+}
