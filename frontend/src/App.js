6cf40f2f648294ce95c10f10e8def956e7e5373d
markoka101 <markoka101@gmail.com>
2023-10-05 15:50:28 -0400

    added login and register page files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,13 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+   } from "react-router-dom";
+import LoginPage from "./Components/LoginPage";
+import RegisterPage from "./Components/RegisterPage";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
 
 export default function App() {
     return null;
