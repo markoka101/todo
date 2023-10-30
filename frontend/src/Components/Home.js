8f4e85d6c64283f509f0adecb7bc8cd125018280
markoka101 <markoka101@gmail.com>
2023-10-30 16:54:19 -0400

    removed popup form for add task, now form expands. Added button to display completed tasks. Display task stats on screen

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,10 +1,10 @@
 import moment from "moment/moment";
 import React, { useEffect } from "react";
-import Popup from 'reactjs-popup';
 
 export default function Home({user}) {
 
     //setting states for form to create new task
+    const [addTaskForm,setAddTaskForm] = React.useState(false);
     const [taskDesc,setTaskDesc] = React.useState('');
     const [date,setDate] = React.useState(moment().format('yyyy-MM-DDTHH:mm').toString());
 
@@ -14,13 +14,13 @@ export default function Home({user}) {
 
     //state for displaying tasks
     const [data,setData] = React.useState();
+
+    //state for displaying completed tasks
+    const [completedData,setCompletedData] = React.useState();
+    const [showComplete,setShowComplete] = React.useState(false);
+
     //refresh when tasks change
     const [refresh, setRefresh] = React.useState(true);
-
-    //states for popup
-    const [open,setOpen] = React.useState(false);
-    const close = () => setOpen(false);
-
 
     //Task object
     const taskObj = {
@@ -47,6 +47,7 @@ export default function Home({user}) {
                 alert('Task Created!');
                 setTaskDesc('');
                 setDate(moment().format('yyyy-MM-DDTHH:mm').toString());
+                setAddTaskForm(false);
                 setRefresh(true);
             } else {
                 alert('Something went wrong :(');
@@ -67,6 +68,7 @@ export default function Home({user}) {
 
     //Display tasks on page load
     useEffect(() => {
+        //current tasks
         const dataFetch = async () => {
             const data = await (
                 await fetch(`http://localhost:8080/user/${user.id}/getTasks`, ({
@@ -78,16 +80,32 @@ export default function Home({user}) {
                     }
                 }))
             ).json();
-
             setData(data);
-            setRefresh(false);
         };
+
+        //completed tasks
+        const completeFetch = async () => {
+            const completedData = await (
+                await fetch(`http://localhost:8080/user/${user.id}/completedTasks`, ({
+                    method: "GET",
+                    mode: "cors",
+                    headers: {
+                        "Content_Type": "application/json",
+                        "Authorization": `Bearer ${user.token}`
+                    }
+                }))
+            ).json();
+            setCompletedData(completedData);
+        };
+
+        setRefresh(false);
+        completeFetch();
         dataFetch();
     }, [user.id, user.token,refresh]);
     
 
-    //temporary function to ensure I am always working with an array thats not null or undefined
-    function tf() {
+    //temporary functions to ensure I am always working with an array thats not null or undefined
+    function taskArr() {
         const arr = [];
         if (data) {
             data.forEach(element => {
@@ -95,6 +113,15 @@ export default function Home({user}) {
             });
         }
         return arr;   
+    }
+    function completedTaskArr() {
+        const arr = [];
+        if (completedData) {
+            completedData.forEach(element => {
+                arr.push(element);
+            })
+        }
+        return arr;
     }
 
     //edit a specfic task
@@ -131,12 +158,50 @@ export default function Home({user}) {
 
     //change states for when edit button is clicked
     function editClick(task) {
+
+        //expands or collapse form based on state
         setEditForm(!editForm);
+
+        //set the inputs of the form as the current task info
         setEditNumber(task.taskNumber);
         setTaskDesc(task.taskDesc);
         setDate(task.date);
     }
+    //change state when add new task button is click
+    function addClick() {
+        setAddTaskForm(!addTaskForm);
+    }
+    //change state of show completed forms
+    function completeClick() {
+        setShowComplete(!showComplete);
+    }
 
+
+    //delete the task
+    function deleteTask(taskNumber) {
+
+        //confirm that user actually wants to delete task
+        if (!window.confirm('This will permenantly delete the task!!')) {
+            return ;
+        }
+
+        fetch(`http://localhost:8080/user/${user.id}/${taskNumber}/delete`, ({
+            method: "DELETE",
+            mode: "cors",
+            headers: {
+                "Authorization": `Bearer ${user.token}`
+            }
+        }))
+        .then(res => {
+            if (res.status === 200) {
+                alert('Task Deleted');
+                setRefresh(true);
+            } else {
+                alert('Something Went Wrong');
+            }
+        })
+        .catch(err => console.log(err));
+    }
 
     //coomplete the task
     function complete(taskNumber) {
@@ -152,6 +217,7 @@ export default function Home({user}) {
             if (res.status === 200) {
                 alert('Task Completed!');
                 setRefresh(true);
+                setShowComplete(false);
             } else {
                 alert('Something Went Wrong :(');
             }
@@ -159,7 +225,31 @@ export default function Home({user}) {
         .catch(err => console.log(err));
     }
 
-    //edit form
+    //new task form
+    function addTask() {
+        return (
+            <div className="bg-gray-500 lg:w-full h-full md:w-full lg:h-4/5 flex flex-col md:mx-auto w-full md:py-4 text-lg font-bold place">
+                <form id="AddForm"onSubmit={handleSubmit} className="flex flex-col md:py-4 text-lg">
+                    <label>
+                        Task Description
+                    </label>
+                    <textarea
+                        value={taskDesc}
+                        onChange={(e) => setTaskDesc(e.target.value)}
+                    />
+                    <label>
+                        Date
+                    </label>
+                    <input type="datetime-local"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}/>
+                    <button type="submit" value="addTask">Add New Task</button>
+                </form>
+            </div>
+        );
+    }
+
+    //edit task form
     function edit()  {
         return (
             <div className="bg-green-300">
@@ -184,35 +274,39 @@ export default function Home({user}) {
         )
     }
 
+    //display list of completed tasks
+    function finishedTasks() {
+        return (
+            <div className="bg-green-500">
+                {completedTaskArr().map((task) => {
+                    return (
+                        <div key={task.taskDesc} className="border-black border-4">
+                            <h1>
+                                {task.taskDesc}
+                            </h1>
+                            {convertDate(formatDate(task.date))} <br></br>
+                        </div>
+                    );
+                })}
+            </div>
+        )
+    }
+
+    /*
+    Currently using different colors to help me visualize the different components 
+    while implementing functionality
+    Design will come after everything is fully functional
+    */
     return (
         <section id="home" className="flex flex-row w-75 h-75 overflow-clip">
-            <div className="container w-1/2 bg-gray-100 mx-auto flex px-10-py-30 md:flex-col flex-col justify-center items-center overflow-auto">
+            <div className="container w-2/3 bg-gray-100 mx-auto flex px-10-py-30 md:flex-col flex-col justify-center items-center overflow-auto">
                 <div className="bg-blue-400 w-full items-center justify-center">
                     {/*Popups the form to create new task when add task is clicked*/}
-                    <Popup position={"right top"}
-                        trigger={<button className="button bg-pink-500">Add Task</button>}>
-                        <div className="bg-gray-500 lg:w-full h-full md:w-full lg:h-4/5 flex flex-col md:mx-auto w-full md:py-4 text-lg font-bold place">
-                            <form id="AddForm"onSubmit={handleSubmit} className="flex flex-col md:py-4 text-lg">
-                                <label>
-                                    Task Description
-                                </label>
-                                <textarea
-                                    value={taskDesc}
-                                    onChange={(e) => setTaskDesc(e.target.value)}
-                                />
-                                <label>
-                                    Date
-                                </label>
-                                <input type="datetime-local"
-                                    value={date}
-                                    onChange={(e) => setDate(e.target.value)}/>
-                                <button type="submit" value="addTask">Add New Task</button>
-                            </form>
-                        </div>
-                    </Popup>
+                    <button onClick={e => addClick()}>Add new task</button>
+                    {addTaskForm === true ? addTask() : null}
                 </div>
-                <div className="bg-orange-300 w-full">
-                    {tf().map((task) => {
+                <div className="bg-orange-300 w-2/3">
+                    {taskArr().map((task) => {
                         return (
                         <div key={task.taskNumber} className="border-black border-4">
                             <h1>
@@ -224,7 +318,9 @@ export default function Home({user}) {
 
                             {convertDate(formatDate(task.date))}<br></br>
                             {task.complete.toString()} <br></br>
-                            {/*Popup edit task form when button is clicked */}
+                            
+                            <button onClick={e => deleteTask(task.taskNumber)}>Delete</button>
+                            {/*Expand edit task form when ediit button is clicked */}
                             <button onClick={e => editClick(task)}>Edit Task</button>
                             <button onClick={e => complete(task.taskNumber)}>Complete</button>
                             {(editForm === true && editNumber === task.taskNumber) ? edit() : null}
@@ -233,7 +329,14 @@ export default function Home({user}) {
                     })}
                 </div>
             </div>
-            <div className="container w-1/2 bg-orange-100 mx-auto flex px-10-py-30 md:flex-row flex-col justify-center items-center"></div>
+            <div className="container w-1/3 bg-orange-100 mx-auto flex px-10-py-30 md:flex-row flex-col justify-center items-center">
+                <div className="bg-rose-200">
+                    <h1>{taskArr().length}</h1>
+                    <h2>{completedTaskArr().length}</h2>
+                    <button onClick={e=>completeClick()}>show completed tasks</button>
+                    {showComplete === true ? finishedTasks() : null}
+                </div>
+            </div>
         </section>
     );
 }
